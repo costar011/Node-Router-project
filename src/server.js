fe730e8a8c0ffe2608767dbd12f33299b2a39491
YERIM <dnaer456@gmail.com>
2020-08-22 13:27:42 +0900

    server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import morgan from "morgan";
 
 // 3000 이라는 port를 담음
 const PORT = 3000;
@@ -9,6 +10,9 @@ const app = express();
 
 // view engine 으로 pug를 세팅.
 app.set("view engine", "pug");
+
+// morgan을 dev모드로 사용할거야.
+app.use(morgan("dev"));
 
 // app.js 에게 CSS나 js는 /assets에 있음
 // app.use(express.static(path.join(__dirname, "/assets")));
